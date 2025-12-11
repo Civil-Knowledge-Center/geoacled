46ce99f1fd5176332e85ea955797fee0d6e8c90c
Sean McLeaish <smcleaish@gmail.com>
2025-12-11 08:33:22 -0500

    update init

--- src/geoacled/__init__.py
+++ src/geoacled/__init__.py
@@ -1,4 +1,5 @@
 """Defines API for acled module."""
 from geoacled.acled.acled_query import AcledMonth, AcledYear
+from geoacled.utils.clean import clean_column, strip_accents
 
-__all__ = ['AcledMonth', 'AcledYear']
+__all__ = ['AcledMonth', 'AcledYear', 'clean_column', 'strip_accents']
